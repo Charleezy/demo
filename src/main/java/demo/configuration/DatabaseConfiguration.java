b60560227f03cfb3dff873782afd6d06aa75b22a
Charlie G <obesechicken13@yahoo.com>
2015-07-23 18:28:34 -0400

    adding database

---
+++ src/main/java/demo/configuration/DatabaseConfiguration.java
@@ -0,0 +1,34 @@
+package demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+
+/**
+ * Created by Charlie on 7/23/2015.
+ */
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("rootroot");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
--- src/main/java/demo/controllers/CharlieController.java
+++ src/main/java/demo/controllers/CharlieController.java
@@ -1,18 +1,42 @@
 package demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by Charlie on 7/18/2015.
  */
-@Controller
+@RestController
 public class CharlieController {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CharlieController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @RequestMapping("/")
-    String home() {
-        //return "Hello World!";
-        return "forward:/index.html";
-        //return View("index.html");
+    List<String> home() {
+        String exampleQuery = "select description from public.conferences";
+        List<String> results = jdbcTemplate.query(
+                exampleQuery,
+                new Object[]{},
+                new RowMapper<String>() {
+                    @Override
+                    public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return rs.getString("description");
+                    }
+                }
+        );
+        return results;
     }
 }
