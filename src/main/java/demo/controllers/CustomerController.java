36b81cf43192550e44d1d95a2cf3a057c79051f1
Charlie G <obesechicken13@yahoo.com>
2015-07-28 16:04:22 -0400

    Refactoring getText methods. Renamed some methods, removed otehr no longer useful ones

--- src/main/java/demo/controllers/CustomerController.java
+++ src/main/java/demo/controllers/CustomerController.java
@@ -3,20 +3,24 @@ package demo.controllers;
 import demo.Customer;
 import demo.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
  * Created by Charlie on 7/18/2015.
  */
-@Controller
+@RestController
 public class CustomerController {
 
     CustomerRepository custRepo;
@@ -42,8 +46,21 @@ public class CustomerController {
         return "saved";
     }
 
-    @RequestMapping(value = "/getText", params = {"text"}, method = RequestMethod.GET)
-    public String getText(String text) {
-        return text;
+//    @RequestMapping(value = "/getText", params = {"text"}, method = RequestMethod.GET)
+//    public String getText(String text) {
+//        return text;
+//    }
+
+    @RequestMapping(value= "/getText",method=RequestMethod.GET, produces="application/json")
+    public ResponseEntity<String> addUser(@ModelAttribute(value="inputText") String inputText, BindingResult result ){
+        ResponseEntity response = new ResponseEntity(HttpStatus.I_AM_A_TEAPOT);
+        if(!result.hasErrors()) {
+            response = response.accepted().headers(new HttpHeaders()).body("{\"statusText\": \"" + inputText + "\"}");
+//            response = new ResponseEntity<>("{\"statusText\": \"" + inputText + "\"}", new HttpHeaders(), HttpStatus.OK);
+//        response.body("blah");
+        }else{
+            response = response.badRequest().headers(new HttpHeaders()).body(null);
+        }
+        return response;
     }
 }
--- src/main/java/demo/controllers/HomeController.java
+++ src/main/java/demo/controllers/HomeController.java
@@ -2,8 +2,9 @@ package demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Charlie on 7/26/2015.
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-//    public String greeting() {
-        model.addAttribute("name", name);
-        return "greeting.html";
+    @RequestMapping("/echo")
+//    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public ModelAndView getText() {
+//        model.addAttribute("name", name);
+//        return "getText.html";
+        ModelAndView modelAndView = new ModelAndView("getText");
+        return modelAndView;
     }
 
 }
